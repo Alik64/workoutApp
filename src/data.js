64697c261b7da0189b1ba803362e23a5dab018e6
Albert <albert.barsamov@gmail.com>
2021-12-31 18:25:31 +0100

    create data.js

---
+++ src/data.js
@@ -0,0 +1,72 @@
+
+
+export const activities = [
+    {
+        name: "Upper body",
+        id: 1,
+        exercices: [
+            "Pushup",
+            "Diamond Push-up",
+            "Pike pushups",
+            "Triceps Box Dip",
+            "Inchworms",
+            "Plank Tap",
+            "Downward Dog to Push-Up",
+            "Decline Push-up",
+            "Incline Push-up",
+            "Bodyweight Triceps Dip",
+            "Superman With Arm Extension"
+
+        ],
+        description: "UpperBody exercises"
+    },
+    {
+        name: "Leg day",
+        id: 2,
+        exercices: [
+            "Bridge",
+            "Squat",
+            "Knee pushup",
+            "Stationary lunge",
+            "Plank to Downward Dog",
+            "Straight-leg donkey kick",
+            "Side-lying hip abduction"
+        ],
+        description: " Don't forget this day!"
+    },
+    {
+        name: "Abs",
+        id: 3,
+        exercices: [
+            "Bicycle crunch",
+            "Plank raise",
+            "Side Plank",
+            "Forearm Plank Reach Out",
+            "text5",
+            "Plank Up-Down",
+            "Lateral Plank Walk",
+            "Body Saw",
+
+        ],
+        description: 'Get your "Six-Pack"'
+    },
+    {
+        name: "Cardio",
+        id: 4,
+        exercices: [
+            "Jamping jack",
+            "Burpees",
+            "Plank Jac",
+        ],
+        description: "You are your only limit"
+
+    },
+    {
+        name: "Full body",
+        id: 5,
+        exercices: [],
+        description: " Let's go buddy !"
+    },
+]
+
+window.activities = activities
