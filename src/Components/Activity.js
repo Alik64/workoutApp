47b3f0d7a4338bc54f6fea8b34512f5d4b10eebe
Albert <albert.barsamov@gmail.com>
2021-12-31 18:25:52 +0100

    activity description

--- src/Components/Activity.js
+++ src/Components/Activity.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { v4 as uuidv4 } from 'uuid'
 
 
 export default function Activity({ activity }) {
@@ -8,15 +8,16 @@ export default function Activity({ activity }) {
 
     return (
         <div>
-            <span>{activity.id}</span>  <h2>{activity.name}</h2>
-            <ul>
-                {activity.exercices.map(ex => {
-                    return <li>{ex}</li>
-                })}
-            </ul>
+            <div>
+                <span>{activity.id}</span>  <h2>{activity.name}</h2>
+                <h2>
+                    {activity.description}
+                </h2>
+            </div>
 
 
 
-        </div>
+
+        </div >
     )
 }
--- src/Context/appContext.js
+++ src/Context/appContext.js
@@ -1,5 +1,5 @@
-import { createContext, useState, useEffect } from "react";
-
+import { createContext } from "react";
+import { activities } from "../data";
 
 export const appContext = createContext()
 
@@ -7,51 +7,11 @@ export const appContext = createContext()
 
 export default function AppContextProvider(props) {
 
-    const activities = [
-        {
-            name: "Upper body",
-            id: 1,
-            exercices: [
-                "upper1",
-                "upper2",
-                "upper3"
-            ]
-        },
-        {
-            name: "Leg day",
-            id: 2,
-            exercices: [
-                "legday1",
-                "legday2",
 
-            ]
-        },
-        {
-            name: "Full body",
-            id: 3,
-            exercices: [
-                "full1",
-                "full2",
-                "full3",
-                "full4"
-            ]
-        },
-        {
-            name: "Abs",
-            id: 4,
-            exercices: [
-                "text1",
-                "text2",
-                "text3",
-                "text4",
-                "text5"
-            ]
-        },
-    ]
-    const [timer, setTimer] = useState()
+
 
     return (
-        <appContext.Provider value={{ timer, activities }}>
+        <appContext.Provider value={{ activities }}>
             {props.children}
         </appContext.Provider>
     )
