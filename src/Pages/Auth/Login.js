8b1d7bcc88a70aa0f91f86e367b0934b0d21f696
Alik64 <albert.barsamov@gmail.com>
2022-09-05 16:31:39 +0200

    refactoring Login.js

--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -1,13 +1,37 @@
 import s from "./Auth.module.css";
+import { useForm } from "react-hook-form";
 
-const Login = () => {
+const LoginScreen = () => {
+  const { register, handleSubmit } = useForm();
+
+  const submitForm = (data) => {
+    console.log(data.email);
+  };
+
   return (
-    <form className={s.root}>
-      <input type="email" />
-      <input type="password" />
-      <button>let's go</button>
+    <form onSubmit={handleSubmit(submitForm)}>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          className="form-input"
+          {...register("email")}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          className="form-input"
+          {...register("password")}
+          required
+        />
+      </div>
+      <button type="submit" className="button">
+        Login
+      </button>
     </form>
   );
 };
-
-export default Login;
+export default LoginScreen;
