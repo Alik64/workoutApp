23f81a132df7fb169617621bdae296d8794541f0
Alik64 <albert.barsamov@gmail.com>
2022-09-09 13:48:24 +0200

    update logo + favicaons

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import { ReactComponent as Logo } from "../../Assets/Images/logo.svg";
-
+// import { ReactComponent as Logo } from "../../Assets/Images/logo.svg";
+import logo from "../../Assets/Images/logo.png";
 import s from "./Home.module.css";
 
 function Home() {
   return (
     <main className={s.root}>
       <section className={s.home_start} data-testid="home_start">
-        <Logo className={s.logo} />
+        <img src={logo} alt="home fit" className={s.logo} />
         <div className={s.links}>
           <Link to={"/register"} className={s.link}>
             Sign up
